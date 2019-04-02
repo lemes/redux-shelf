1e1de1f1cbc19d3595464ec27f6f798d7ba4e027
Guilherme Popolin <guilhermespopolin@gmail.com>
2019-04-02 04:22:01 -0300

    refactor: removing unecessary files

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export function entries(obj = {}) {
-  return Object.keys(obj).map((key) => [key, obj[key]])
-}
-
-export function guaranteeArray(param = []) {
-  return Array.isArray(param) ? param : [param]
-}
