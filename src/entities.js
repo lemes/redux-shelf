2ece4e5864d9cb5fb8668a9008b8a4314e5d412a
Guilherme Popolin <guilhermespopolin@gmail.com>
2019-04-16 22:45:19 -0300

    refact(entities)

--- src/entities.js
+++ src/entities.js
@@ -1,11 +1,26 @@
-import { normalize } from 'normalizr'
+import { normalize, schema } from 'normalizr'
+import has from 'lodash.has'
+import pick from 'lodash.pick'
+import isString from 'lodash.isstring'
 
-import { methods } from './constants'
+import { methods, errorMessages } from './constants'
 
-export const generateActionType = (operation, resourceType, resourceId) =>
-  `entities/${operation}/${resourceType.toUpperCase()}${
+/**
+ *
+ * @param {string} operation - Operation that the action describes (SET, UPDATE or REMOVE).
+ * @param {string} resourceType - Resource type name. Requires a non empty string.
+ * @param {string} resourceId  - Resource identifier. Should be provided when the opertion
+ * is under an specific entity.
+ */
+export const generateActionType = (operation, resourceType, resourceId) => {
+  if (!isString(resourceType) || resourceType === '') {
+    throw new Error(errorMessages.invalidResourceType)
+  }
+
+  return `entities/${operation}/${resourceType.toUpperCase()}${
     resourceId ? `:${resourceId}` : ''
   }`
+}
 
 /**
  * Returns an action that represents the addition of one or more entities on
@@ -13,20 +28,32 @@ export const generateActionType = (operation, resourceType, resourceId) =>
  * be used payload on third argument normalization.
  *
  * @param {string} resourceType - Resouce type name.
- * @param {object} schema - Entity's schema.
+ * @param {object} schm - Entity's schema. Should be a normalizr entity schema object.
  * @param {object|object[]} payload - Payload to be stored on state.
  */
-function set(resourceType, schema, payload) {
+function set(resourceType, schm, payload) {
+  // validate provided schema
+  if (!(schm instanceof schema.Entity)) {
+    throw new Error(errorMessages.invalidNormalizrSchema)
+  }
+
   return {
     type: generateActionType(methods.SET, resourceType),
     meta: {
       resourceType,
       method: methods.SET,
     },
-    payload: normalize(payload, schema).entities,
+    payload: normalize(payload, schm).entities,
   }
 }
 
+/**
+ * Returns an action that represents the updating of one specific entity.
+ *
+ * @param {string} resourceType - Resouce type name.
+ * @param {string} resourceId - Resource identifier.
+ * @param {object|object[]} payload - Data to be used on update operation.
+ */
 function update(resourceType, resourceId, payload) {
   return {
     type: generateActionType(methods.UPDATE, resourceType, resourceId),
@@ -35,13 +62,20 @@ function update(resourceType, resourceId, payload) {
       resourceType,
       resourceId,
     },
-    payload: normalize(payload).entities,
+    payload,
   }
 }
 
+/**
+ * Returns an action that represents the updating of one specific entity.
+ *
+ * @param {string} resourceType - Resouce type name.
+ * @param {string} resourceId - Resource identifier.
+ * @param {object|object[]} payload - Data to be used on update operation.
+ */
 function remove(resourceType, resourceId) {
   return {
-    type: generateActionType(methods.REMOVE),
+    type: generateActionType(methods.REMOVE, resourceType, resourceId),
     meta: {
       method: methods.REMOVE,
       resourceType,
@@ -50,6 +84,13 @@ function remove(resourceType, resourceId) {
   }
 }
 
+/**
+ * Entity reducer. Implements handlers logic for entities operations: set, update
+ * and remove.
+ *
+ * @param {string} state - Current application state.
+ * @param {object} action - Action that informs wich operation should be realized.
+ */
 function reducer(state = {}, { type, meta, payload }) {
   if (!type.startsWith('entities') || !(meta && meta.resourceType)) {
     return state
@@ -65,7 +106,39 @@ function reducer(state = {}, { type, meta, payload }) {
         state,
       )
     case methods.UPDATE:
+      return has(state, `${meta.resourceType}.${meta.resourceId}`)
+        ? {
+            ...state,
+            [meta.resourceType]: {
+              ...state[meta.resourceType],
+              [meta.resourceId]: payload,
+            },
+          }
+        : state
     case methods.REMOVE:
+      if (has(state, `${meta.resourceType}.${meta.resourceId}`)) {
+        // should remove all resourceType node from state
+        if (Object.keys(state[meta.resourceType]).length === 1) {
+          return pick(
+            state,
+            Object.keys(state).filter(
+              (resourceType) => resourceType !== meta.resourceType,
+            ),
+          )
+        }
+
+        return {
+          ...state,
+          [meta.resourceType]: pick(
+            state[meta.resourceType],
+            Object.keys(state[meta.resourceType]).filter(
+              (resourceId) => resourceId !== meta.resourceId,
+            ),
+          ),
+        }
+      }
+
+      return state
     default:
       return state
   }
--- test/entities.spec.js
+++ test/entities.spec.js
@@ -1,20 +1,148 @@
 import { schema, normalize } from 'normalizr'
 
-import { entities } from '../src'
+import { errorMessages } from '../src/constants'
+import entities, { generateActionType } from '../src/entities'
 
-const userSchema = new schema.Entity('users')
-const articleSchema = new schema.Entity('articles', {
-  author: userSchema,
+describe('Generating an action type, given operation, resourceType and resourceId information', () => {
+  describe('When resourceType is not provided or is invalid (non empty string)', () => {
+    // For error assertions it's required that code that will throw the error be wrapper
+    // by a function
+    it('should throw an error informing that', () => {
+      expect(() => generateActionType('SET', undefined)).toThrowError(
+        errorMessages.invalidResourceType,
+      )
+      expect(() => generateActionType('SET', '')).toThrowError(
+        errorMessages.invalidResourceType,
+      )
+    })
+  })
+
+  describe('When operation that action represents is about a entity set', () => {
+    it('it should return the action name following the format: `entities/<operation>/<RESOURCE_TYPE>`', () => {
+      const action = generateActionType('SET', 'users')
+      expect(action).toBe('entities/SET/USERS')
+    })
+  })
+
+  describe('When operation that action represents is about an specific entity', () => {
+    it('it should return the action name following the format: `entities/<operation>/<RESOURCE_TYPE>:<resourceiId>`', () => {
+      const action = generateActionType('SET', 'users', 'abc123')
+      expect(action).toBe('entities/SET/USERS:abc123')
+    })
+  })
 })
 
 describe('entities', () => {
-  let action
-  let currentState
-  let payload
-  let state
+  describe('Action creators', () => {
+    describe('set', () => {
+      const userSchema = new schema.Entity('users')
+      const articlesSchema = new schema.Entity('articles', {
+        author: userSchema,
+      })
+      const payload = { id: '42', name: 'Arthur' }
 
-  beforeEach(() => {
-    currentState = {
+      describe('When schema provided is a invalid one (not a normalizr entity schema object)', () => {
+        it('should throw an error informing that', () => {
+          expect(() =>
+            entities.set('articles', {}, { id: '1', title: 'Some article' }),
+          ).toThrowError(errorMessages.invalidNormalizrSchema)
+        })
+      })
+
+      describe('When payload provided is invalid for normalization', () => {
+        it('should thrown an error informing that', () => {
+          expect(() =>
+            entities.set('articles', articlesSchema, null),
+          ).toThrowError()
+        })
+      })
+
+      describe('When resource type provided is invalid (not a non empty string)', () => {
+        it('should thrown an error informing that', () => {
+          expect(() =>
+            entities.set(undefined, articlesSchema, payload),
+          ).toThrowError(errorMessages.invalidResourceType)
+          expect(() => entities.set('', articlesSchema, payload)).toThrowError(
+            errorMessages.invalidResourceType,
+          )
+        })
+      })
+
+      describe('When resouceType, schema and payload provided are valid', () => {
+        it('should return an action with normalized payload that represents the setting data on state operation', () => {
+          expect(entities.set('articles', articlesSchema, payload)).toEqual({
+            type: 'entities/SET/ARTICLES',
+            meta: {
+              resourceType: 'articles',
+              method: 'SET',
+            },
+            payload: normalize(payload, articlesSchema).entities,
+          })
+        })
+      })
+    })
+
+    describe('update', () => {
+      describe('When resource type provided is invalid (not a non empty string)', () => {
+        it('should thrown an error informing that', () => {
+          expect(() =>
+            entities.update(undefined, 42, { id: 42, name: 'Arthur' }),
+          ).toThrowError(errorMessages.invalidResourceType)
+          expect(() =>
+            entities.update('', 42, { id: 42, name: 'Arthur' }),
+          ).toThrowError(errorMessages.invalidResourceType)
+        })
+      })
+
+      describe('When resourceType, resourceId and payload provided are valid information', () => {
+        it('should return an action that represents updating the entity data on state operation', () => {
+          expect(
+            entities.update('users', '1', { id: '1', name: 'Thomas' }),
+          ).toEqual({
+            type: 'entities/UPDATE/USERS:1',
+            meta: {
+              resourceType: 'users',
+              resourceId: '1',
+              method: 'UPDATE',
+            },
+            payload: {
+              id: '1',
+              name: 'Thomas',
+            },
+          })
+        })
+      })
+    })
+
+    describe('remove', () => {
+      describe('When resource type provided is invalid (not a non empty string)', () => {
+        it('should thrown an error informing that', () => {
+          expect(() => entities.remove(undefined, '42')).toThrowError(
+            errorMessages.invalidResourceType,
+          )
+          expect(() => entities.remove('', '42')).toThrowError(
+            errorMessages.invalidResourceType,
+          )
+        })
+      })
+
+      describe('When resourceType, resourceId provided are valid information', () => {
+        it('should return an action that represents remotion of the entity data from state operation', () => {
+          expect(entities.remove('users', 1)).toEqual({
+            type: 'entities/REMOVE/USERS:1',
+            meta: {
+              resourceType: 'users',
+              resourceId: 1,
+              method: 'REMOVE',
+            },
+          })
+        })
+      })
+    })
+  })
+
+  describe('Reducer', () => {
+    const currentState = {
       articles: {
         abc123: {
           id: 'abc123',
@@ -23,150 +151,147 @@ describe('entities', () => {
         },
       },
       users: {
-        2: { name: 'Lisa Silva' },
-        3: { name: 'Marcus Paiva' },
+        2: { id: 2, name: 'Lisa Silva' },
+        3: { id: 3, name: 'Marcus Paiva' },
       },
     }
-    payload = {
-      id: 'abc123',
-      title: 'First article',
-      author: {
-        id: 2,
-        name: 'Lisa Silva',
-      },
-    }
-  })
 
-  describe.skip('when deleting an entity', () => {
-    let userIdDeleted
-
-    beforeEach(() => {
-      userIdDeleted = 2
-      action = entities.remove('users', userIdDeleted)
-    })
-
-    describe('action', () => {
-      it('should return action type `entities/users/REMOVE`', () => {
-        expect(action.type).toBe('entities/users/REMOVE')
-      })
-
-      it('should return meta selector', () => {
-        expect(action.meta.selector).toBe(userIdDeleted)
-      })
-
-      it('should return meta method REMOVE', () => {
-        expect(action.meta.method).toBe('REMOVE')
-      })
-
-      it('should return meta type users', () => {
-        expect(action.meta.type).toBe('users')
+    describe('On state initialization', () => {
+      it('should initialize state as empty object', () => {
+        const state = entities.reducer(undefined, { type: '@@INIT' })
+        expect(state).toEqual({})
       })
     })
-  })
 
-  describe('when setting an entity', () => {
-    beforeEach(() => {
-      action = entities.set('articles', articleSchema, payload)
-    })
-
-    describe('action', () => {
-      it('should return action type `entities/SET/ARTICLES`', () => {
-        expect(action.type).toBe('entities/SET/ARTICLES')
-      })
-
-      it('should return a normalized version of provided payload', () => {
-        expect(action.payload).toEqual(
-          normalize(payload, articleSchema).entities,
-        )
-      })
-
-      it('should return meta method SET', () => {
-        expect(action.meta.method).toBe('SET')
-      })
-
-      it('should return meta type users', () => {
-        expect(action.meta.resourceType).toBe('articles')
+    describe('When action provided does not represent entities tasks', () => {
+      it('should return current state', () => {
+        const newState = entities.reducer(currentState, {
+          type: 'SOME_ACTION_TYPE',
+        })
+        expect(newState).toBe(currentState)
       })
     })
-  })
 
-  describe.skip('when updating an entity', () => {
-    beforeEach(() => {
-      action = entities.update('users', payload)
-    })
-
-    describe('action', () => {
-      it('should return action type `entities/users/UPDATE`', () => {
-        expect(action.type).toBe('entities/users/UPDATE')
-      })
-
-      it('should return given value as payload', () => {
-        expect(action.payload).toBe(payload)
-      })
-
-      it('should return meta method UPDATE', () => {
-        expect(action.meta.method).toBe('UPDATE')
-      })
-
-      it('should return meta type users', () => {
-        expect(action.meta.type).toBe('users')
-      })
-    })
-  })
-
-  describe('given an unhandled action', () => {
-    it('should keep state unchanged', () => {
-      expect(
-        entities.reducer(currentState, {
-          type: 'NOT_HANDLED',
-          meta: { type: 'users', method: 'UPDATE' },
-        }),
-      ).toBe(currentState)
-    })
-  })
-
-  describe('given an unhandled action without meta', () => {
-    it('should keep state unchanged', () => {
-      expect(
-        entities.reducer(currentState, { type: 'entities/users/UPDATE' }),
-      ).toBe(currentState)
-    })
-  })
-
-  describe('given an unhandled action without meta type', () => {
-    it('should keep state unchanged', () => {
-      expect(
-        entities.reducer(currentState, {
-          type: 'entities/users/UPDATE',
+    describe('When entity action provided is not associated with a resourceType', () => {
+      it('should return current state', () => {
+        const newState = entities.reducer(currentState, {
+          type: 'entities/SET/USERS',
           meta: {},
-        }),
-      ).toBe(currentState)
-    })
-  })
-
-  describe('given an unhandled action method', () => {
-    it('should keep state unchanged', () => {
-      expect(
-        entities.reducer(currentState, {
-          type: 'entities/users/NOT_HANDLED_METHOD',
-          meta: {
-            method: 'NOT_HANDLED',
-            type: 'users',
-          },
-        }),
-      ).toBe(currentState)
-    })
-  })
-
-  describe('given an action that changes state', () => {
-    beforeEach(() => {
-      action = entities.set('articles', articleSchema, payload)
-      state = entities.reducer(undefined, action)
+        })
+        expect(newState).toBe(currentState)
+      })
     })
 
-    it('should update state', () => {
-      expect(state).toEqual({
-        ...normalize(payload, articleSchema).entities,
+    describe('When action provided represents entities tasks and it is associated with a resourceType', () => {
+      describe('On setting entity data on state', () => {
+        it('should set entity data on state properly', () => {
+          const userSchema = new schema.Entity('users')
+          const payload = { id: 42, name: 'Arthur' }
+          const action = entities.set('users', userSchema, payload)
+          const newState = entities.reducer(currentState, action)
+          expect(newState).toEqual({
+            articles: {
+              abc123: {
+                id: 'abc123',
+                title: 'First article',
+                author: 2,
+              },
+            },
+            users: {
+              42: { id: 42, name: 'Arthur' },
+            },
+          })
+        })
+      })
+
+      describe('On updating entity data on state', () => {
+        describe('When entity to be updated does not exist on state', () => {
+          it('should return current state', () => {
+            const payload = { id: 2, name: 'Bill' }
+            const action = entities.update('authors', 2, payload)
+            const newState = entities.reducer(currentState, action)
+            expect(newState).toBe(currentState)
+          })
+        })
+
+        describe('When entity to be updated exists on state', () => {
+          it('should update entity properly', () => {
+            const payload = { id: 2, name: 'Mario' }
+            const action = entities.update('users', 2, payload)
+            const newState = entities.reducer(currentState, action)
+            expect(newState).toEqual({
+              articles: {
+                abc123: {
+                  id: 'abc123',
+                  title: 'First article',
+                  author: 2,
+                },
+              },
+              users: {
+                2: { id: 2, name: 'Mario' },
+                3: { id: 3, name: 'Marcus Paiva' },
+              },
+            })
+          })
+        })
+      })
+
+      describe('On removing entity data from state', () => {
+        describe('When entity to be removed does not exist on state', () => {
+          it('should return current state', () => {
+            const action = entities.remove('users', 45)
+            const newState = entities.reducer(currentState, action)
+            expect(newState).toBe(currentState)
+          })
+        })
+
+        describe('When entity to be removed exists on state', () => {
+          describe('When entity to be removed is the last one of that entity type', () => {
+            it('should remove all entity type node from state', () => {
+              const action = entities.remove('articles', 'abc123')
+              const newState = entities.reducer(currentState, action)
+              expect(newState).toEqual({
+                users: {
+                  2: { id: 2, name: 'Lisa Silva' },
+                  3: { id: 3, name: 'Marcus Paiva' },
+                },
+              })
+            })
+          })
+
+          describe('When entity to be remove is not the last one of that entity type', () => {
+            it('should remove only the specified entity from entity type node', () => {
+              const action = entities.remove('users', '2')
+              const newState = entities.reducer(currentState, action)
+              expect(newState).toEqual({
+                articles: {
+                  abc123: {
+                    id: 'abc123',
+                    title: 'First article',
+                    author: 2,
+                  },
+                },
+                users: {
+                  3: { id: 3, name: 'Marcus Paiva' },
+                },
+              })
+            })
+          })
+        })
+      })
+
+      describe('When an unknown entity action is provided', () => {
+        it('should return current state', () => {
+          const action = {
+            type: 'entities/UNKNOWN_ACTION_TYPE',
+            meta: {
+              resourceType: 'users',
+              method: 'UNKNOWN',
+            },
+          }
+          const newState = entities.reducer(currentState, action)
+          expect(newState).toBe(currentState)
+        })
       })
     })
   })
